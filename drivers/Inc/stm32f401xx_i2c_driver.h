def35b197abbe1a4ee090a121b7e2b9bda27a408
Rafaela Freitas <58916022+Rafaelatff@users.noreply.github.com>
2022-12-26 17:23:58 -0300

    Add files via upload
    updated i2c header file up to 197

--- drivers/Inc/stm32f401xx_i2c_driver.h
+++ drivers/Inc/stm32f401xx_i2c_driver.h
@@ -47,6 +47,27 @@ typedef struct{
 #define I2C_FM_DUTY_2			0
 #define I2C_FM_DUTY_16_9		1
 
+/*
+ * 	I2C related status flag definitions
+ */
+#define I2C_FLAG_SB 			(1 << I2C_SR1_SB)
+#define I2C_FLAG_ADDR 			(1 << I2C_SR1_ADDR)
+#define I2C_FLAG_BTF 			(1 << I2C_SR1_BTF)
+// #define I2C_ADD10_FLAG 		(1 << I2C_SR1_ADD10)
+#define I2C_FLAG_STOPF 			(1 << I2C_SR1_STOPF)
+#define I2C_FLAG_RXNE 			(1 << I2C_SR1_RXNE)
+#define I2C_FLAG_TXE			(1 << I2C_SR1_TXE)
+#define I2C_FLAG_BERR 			(1 << I2C_SR1_BERR)
+#define I2C_FLAG_ARLO 			(1 << I2C_SR1_ARLO)
+#define I2C_FLAG_AF 			(1 << I2C_SR1_AF)
+#define I2C_FLAG_OVR 			(1 << I2C_SR1_OVR)
+// #define I2C_FLAG_PECERR 		(1 << I2C_SR1_PECERR)
+#define I2C_FLAG_TIMEOUT 		(1 << I2C_SR1_TIMEOUT)
+// #define I2C_FLAG_SMBALERT 	(1 << I2C_SR1_AMBALERT)
+
+#define I2C_DISABLE_SR  	RESET
+#define I2C_ENABLE_SR   	SET
+
 /*******************************************************
  * 			API supported by this driver
  *
@@ -60,12 +81,13 @@ void I2C_PeriClkCtrl(I2C_RegDef_t *pSPIx, uint8_t EnOrDi);
 /*
  *  Init and De-Init
  */
-void I2C_Init(I2C_Handle_t *pSPIHandle);
+void I2C_Init(I2C_Handle_t *pI2CHandle);
 void I2C_DeInit(I2C_RegDef_t *pI2Cx);
 
 /*
  *  Data Send and Receive
  */
+void I2C_MasterSendData(I2C_Handle_t *pI2CHandle,uint8_t *pTxbuffer, uint32_t Len, uint8_t SlaveAddr,uint8_t Sr);
 
 
 /*
@@ -79,7 +101,10 @@ void I2C_IRQPriorityConfig(uint8_t IRQNumber, uint32_t IRQPriority);
  */
 void I2C_PeripheralControl(I2C_RegDef_t *pI2Cx, uint8_t EnOrDi);
 uint8_t I2C_GetFlagStatus(I2C_RegDef_t *pI2Cx, uint32_t FlagName);
-
+void I2C_GenerateStopCondition(I2C_RegDef_t *pI2Cx);
+void I2C_PeripheralControl(I2C_RegDef_t *pI2Cx, uint8_t EnOrDi);
+uint32_t RCC_GetPCLK1Value(void);
+uint32_t RCC_GetPLLOutputClock (void);
 
 /*
  * 	Application callback
